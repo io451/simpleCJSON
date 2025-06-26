4726b8c69e8aa4cb2e841ee4089b6ce8adb3d8a7
IO <prvtangl@gmail.com>
2025-06-26 06:36:53 +0300

    Few improvements and 3 new functions

--- sJS.c
+++ sJS.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+<<<<<<< HEAD
 #if (sJSON_MAJOR_VERSION != 0 || sJSON_MINOR_VERSION != 1 || sJSON_PATCH_VERION != 0)
     #error sJSON.c and sJSON.h have a different versions. Please get a correct version of sJSON
 #endif
@@ -15,12 +16,21 @@
 int sJSGetNumberValue(const char *filename, const char *block, const char *value)
 {
     if (!filename || !block || !value) return -1;
+=======
+int sJSGetNumberValue(const char *filename, const char *object, const char *value)
+{
+    if (!filename || !object || !value) return -1;
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
 
     FILE *f = fopen(filename,"rb");
 
     if(!f)
     {
+<<<<<<< HEAD
         printf("[sJSON] Unable to open %s, file do not exist or filepath is wrong\n",filename);
+=======
+        perror("sJSGetNumberValue: Unable to open a file. File do not exist or filepath is wrong\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         return -1;
     }
 
@@ -31,11 +41,24 @@ int sJSGetNumberValue(const char *filename, const char *block, const char *value
     char *buffer = malloc(filesize + 1);
     if(!buffer)
     {
+<<<<<<< HEAD
+=======
+        perror("sJSGetNumberValue: Failed to malloc the memory for buffer\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         return -1;
     }
 
     size_t read = fread(buffer,sizeof(char),filesize,f);
+<<<<<<< HEAD
     if(read != filesize) {free(buffer); fclose(f); return -1;}
+=======
+    if(read != filesize) 
+    {
+        free(buffer); 
+        fclose(f); 
+        return -1;
+    }
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
     buffer[filesize] = '\0';
     fclose(f);
 
@@ -44,6 +67,7 @@ int sJSGetNumberValue(const char *filename, const char *block, const char *value
 
     if(!root)
     {
+<<<<<<< HEAD
         return -1;
     }
 
@@ -51,6 +75,17 @@ int sJSGetNumberValue(const char *filename, const char *block, const char *value
     
     if(!item || !cJSON_IsObject(item))
     {
+=======
+        perror("sJSGetNumberValue: Failed to parse JSON\n");
+        return -1;
+    }
+
+    cJSON *item = cJSON_GetObjectItem(root,object);
+    
+    if(!item || !cJSON_IsObject(item))
+    {
+        perror("sJSGetNumberValue: Object do not exist or incorrect\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         cJSON_Delete(root);
         return -1;
     }
@@ -59,6 +94,11 @@ int sJSGetNumberValue(const char *filename, const char *block, const char *value
 
     if(!res || !cJSON_IsNumber(res))
     {
+<<<<<<< HEAD
+=======
+        
+        perror("sJSGetNumberValue: Value do not exist, or not a number, or incorrect\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         cJSON_Delete(root);
         return -1;
     }
@@ -69,15 +109,25 @@ int sJSGetNumberValue(const char *filename, const char *block, const char *value
     return result;
 }
 
+<<<<<<< HEAD
 char *sJSGetString(const char *filename, const char *block, const char *value)
 {
     if(!filename || !block || !value) return NULL;
+=======
+char *sJSGetStringValue(const char *filename, const char *object, const char *value)
+{
+    if(!filename || !object || !value) return NULL;
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
 
     FILE *f = fopen(filename,"rb");
 
     if(!f)
     {
+<<<<<<< HEAD
         printf("Не удалось открыть файл\n");
+=======
+        perror("sJSGetStringValue: Unable to open a file. File do not exist or filepath is wrong\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         return NULL;
     }
 
@@ -88,12 +138,26 @@ char *sJSGetString(const char *filename, const char *block, const char *value)
     char *buffer = malloc(filesize + 1);
     if(!buffer)
     {
+<<<<<<< HEAD
         printf("Не удалось выделить память\n");
+=======
+        perror("sJSGetStringValue: Failed to malloc the memory for buffer\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         return NULL;
     }
 
     size_t read = fread(buffer,sizeof(char),filesize,f);
+<<<<<<< HEAD
     if(read != filesize) {printf("Не удалось прочитать файл\n");free(buffer); fclose(f); return NULL;}
+=======
+    if(read != filesize) 
+    {
+        perror("sJSGetStringValue: Failed to read file\n");
+        free(buffer); 
+        fclose(f); 
+        return NULL;
+    }
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
     buffer[filesize] = '\0';
     fclose(f);
 
@@ -102,6 +166,7 @@ char *sJSGetString(const char *filename, const char *block, const char *value)
 
     if(!root)
     {
+<<<<<<< HEAD
         printf("Не удалось пропарсить файл\n");
         return NULL;
     }
@@ -111,6 +176,17 @@ char *sJSGetString(const char *filename, const char *block, const char *value)
     if(!item || !cJSON_IsObject(item))
     {
         printf("Не удалось получить объект\n");
+=======
+        perror("sJSGetStringValue: Failed to parse JSON\n");
+        return NULL;
+    }
+
+    cJSON *item = cJSON_GetObjectItem(root,object);
+
+    if(!item || !cJSON_IsObject(item))
+    {
+        perror("sJSGetStringValue: Object do not exist or incorrect\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         cJSON_Delete(root);
         return NULL;
     }
@@ -119,7 +195,11 @@ char *sJSGetString(const char *filename, const char *block, const char *value)
 
     if(!res || !cJSON_IsString(res))
     {
+<<<<<<< HEAD
         printf("Не удалось получить значение\n");
+=======
+        perror("sJSGetStringValue: Value do not exist, or not a string, or incorrect\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         cJSON_Delete(root);
         return NULL;
     }
@@ -131,14 +211,24 @@ char *sJSGetString(const char *filename, const char *block, const char *value)
     return result;
 }
 
+<<<<<<< HEAD
 double sJSGetDoubleValue(const char *filename, const char *block, const char *value)
 {
     if (!filename || !block || !value) return -1;
+=======
+double sJSGetDoubleValue(const char *filename, const char *object, const char *value)
+{
+    if (!filename || !object || !value) return -1;
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
     FILE *f = fopen(filename,"rb");
 
     if(!f)
     {
+<<<<<<< HEAD
         printf("[sJSON] Unable to open %s, file do not exist or filepath is wrong\n",filename);
+=======
+        perror("sJSGetDoubleValue: Unable to open a file\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         return -1;
     }
 
@@ -149,11 +239,25 @@ double sJSGetDoubleValue(const char *filename, const char *block, const char *va
     char *buffer = malloc(filesize + 1);
     if(!buffer)
     {
+<<<<<<< HEAD
+=======
+        perror("sJSGetDoubleValue: Failed to malloc a memory for buffers\n");
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
         return -1;
     }
 
     size_t read = fread(buffer,sizeof(char),filesize,f);
+<<<<<<< HEAD
     if(read != filesize) {free(buffer); fclose(f); return -1;}
+=======
+    if(read != filesize) 
+    {
+        perror("sJSGetDoubleValue: Failed to read a file\n");
+        free(buffer); 
+        fclose(f); 
+        return -1;
+    }
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
     buffer[filesize] = '\0';
     fclose(f);
 
@@ -165,7 +269,11 @@ double sJSGetDoubleValue(const char *filename, const char *block, const char *va
         return -1;
     }
 
+<<<<<<< HEAD
     cJSON *item = cJSON_GetObjectItem(root,block);
+=======
+    cJSON *item = cJSON_GetObjectItem(root,object);
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
     
     if(!item || !cJSON_IsObject(item))
     {
@@ -185,4 +293,112 @@ double sJSGetDoubleValue(const char *filename, const char *block, const char *va
     
     cJSON_Delete(root);
     return result;
+<<<<<<< HEAD
+=======
+}
+
+cJSON *sJSParseJSON(const char *filename)
+{
+    if(!filename) return NULL;
+
+    FILE *f = fopen(filename,"rb");
+
+    if(!f)
+    {
+        perror("sJSParseJSON: Unable to open a file. File do not exist or filepath is wrong\n");
+        return -1;
+    }
+
+    fseek(f,0,SEEK_END);
+    long filesize = ftell(f);
+    rewind(f);
+
+    char *buffer = malloc(filesize + 1);
+    if(!buffer)
+    {
+        perror("sJSParseJSON: Failed to malloc the memory for buffer\n");
+        return -1;
+    }
+
+    size_t read = fread(buffer,sizeof(char),filesize,f);
+    if(read != filesize) 
+    {
+        perror("sJSParseJSON: Failed to read a file\n");
+        free(buffer); 
+        fclose(f); 
+        return -1;
+    }
+    buffer[filesize] = '\0';
+    fclose(f);
+
+    cJSON *root = cJSON_Parse(buffer);
+    free(buffer);
+    return root;
+}
+
+
+int sJSGetNumber(cJSON *root, const char *object, const char *value)
+{
+    if (!root)
+    {
+        perror("sJSGetNumber: cJSON is not parsed (cJSON is NULL) \n");
+        return -1;
+    }
+    cJSON *item = cJSON_GetObjectItem(root,object);
+    
+    if(!item || !cJSON_IsObject(item))
+    {
+        perror("sJSGetNumber: Object do not exist or incorrect\n");
+        cJSON_Delete(root);
+        return -1;
+    }
+
+    cJSON *res = cJSON_GetObjectItem(item,value);
+
+    if(!res || !cJSON_IsNumber(res))
+    {
+        
+        perror("sJSGetNumber: Value do not exist, or not a number, or incorrect\n");
+        cJSON_Delete(root);
+        return -1;
+    }
+
+    int result = (int)cJSON_GetNumberValue(res);
+    
+    cJSON_Delete(root);
+    return result;
+}
+
+char *sJSGetString(cJSON *root, const char *object, const char *value)
+{
+    if (!root)
+    {
+        perror("sJSGetString: cJSON is not parsed (cJSON is NULL) \n");
+        return -1;
+    }
+
+    cJSON *item = cJSON_GetObjectItem(root,object);
+
+    if(!item || !cJSON_IsObject(item))
+    {
+        perror("sJSGetString: Object do not exist or incorrect\n");
+        cJSON_Delete(root);
+        return NULL;
+    }
+
+    cJSON *res = cJSON_GetObjectItem(item,value);
+
+    if(!res || !cJSON_IsString(res))
+    {
+        perror("sJSGetString: Value do not exist, or not a string, or incorrect\n");
+        cJSON_Delete(root);
+        return NULL;
+    }
+    
+    char *result = strdup(cJSON_GetStringValue(res));
+
+    cJSON_Delete(root);
+
+    return result;
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
 }
--- sJS.h
+++ sJS.h
@@ -16,6 +16,7 @@ Author: https://github.com/io451
 
 
 #define sJSON_MAJOR_VERISON 0
+<<<<<<< HEAD
 #define sJSON_MINOR_VERSION 1
 #define sJSON_PATCH_VERSION 0
 
@@ -23,6 +24,16 @@ Author: https://github.com/io451
  * @brief Automaticly open a file, read JSON data and return a specific number value from JSON string in object
  * 
  * Note: Do not use this function with FOR or WHILE loops. For loop operations use sJSGetNumber
+=======
+#define sJSON_MINOR_VERSION 0
+#define sJSON_PATCH_VERSION 2
+
+/**
+ * @brief Automaticly open a file, read JSON data and 
+ * return a specific integer number from JSON string in object
+ * 
+ * @note Do not use this function with FOR or WHILE loops. For loop operations use sJSGetNumber
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
  * 
  * @param filename  path to JSON file
  * @param block  JSON object which contain a value string
@@ -34,9 +45,16 @@ int sJSGetNumberValue(const char *filename, const char *block, const char *value
 
 
 /**
+<<<<<<< HEAD
  * @brief Automaticly open a file, read JSON data and return a specific number value as double from JSON string in object
  * 
  * Note: Do not use this function with FOR or WHILE loops. For loop operations use sJSGetNumber
+=======
+ * @brief Automaticly open a file, read JSON data and return a double number 
+ * from JSON string in object
+ * 
+ * @note Do not use this function with FOR or WHILE loops. For loop operations use sJSGetNumber
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
  * 
  * @param filename  path to JSON file
  * @param block  JSON object which contain a value string
@@ -58,9 +76,63 @@ double sJSGetDoubleValue(const char *filename, const char *block, const char *va
  * 
  * @return char *  from JSON string, or NULL on failure
  */
+<<<<<<< HEAD
 char *sJSGetString(const char *filename, const char *block, const char *value);
 
 
+=======
+char *sJSGetStringValue(const char *filename, const char *block, const char *value);
+
+
+/**
+ * @brief Opens a file, and parse JSON data in cJSON struct
+ * 
+ * You can use this function for more specific operations.
+ * 
+ * @param filename JSON file to parse
+ * 
+ * @return cJSON struct or NULL on failure
+ */
+cJSON *sJSParseJSON(const char *filename);
+
+
+/**
+ * @brief Alternative to sJSGetNumberValue.
+ * 
+ * sJSGetNumber gets a number value from cJSON object, you can use
+ * this function in FOR and WHILE loops, because sJSGetNumber do not 
+ * opens a file.
+ * 
+ * @param root cJSON struct with parsed file, you can use
+ * @param object a JSON object to parse
+ * @param value a JSON string to get integer
+ * 
+ * @return integer number or -1 on failure
+ * 
+ * 
+ * @note You can use sJSParseJSON to get cJSON struct
+ */
+int sJSGetNumber(cJSON *root, const char *object, const char *value);
+
+
+/**
+ * @brief Alternative to sJSGetStringValue.
+ * 
+ * sJSGetString gets a string value from cJSON object, you can use
+ * this function in FOR and WHILE loops, because sJSGetString do not 
+ * opens a file.
+ * 
+ * @param root cJSON struct with parsed file, you can use
+ * @param object a JSON object to parse
+ * @param value a JSON string to get integer
+ * 
+ * @return string or NULL on failure
+ * 
+ * 
+ * @note You can use sJSParseJSON to get cJSON struct
+ */
+char *sJSGetString(cJSON *root, const char *object, const char *value);
+>>>>>>> 483b1fa (A few improvemets and 3 new functions)
 #endif
 
 
